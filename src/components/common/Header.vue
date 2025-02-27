80690b7782c177b85e6d6e31fd918bfb353da050
Aung Paing Soe <aungpaingsoedev@gmail.com>
2025-02-27 06:37:12 +0630

    Sidebar

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -4,51 +4,79 @@ const { collapsed } = defineProps(["collapsed"]);
 
 <template>
   <a-layout-header
-    class="rounded-lg shadow-sm sticky top-0 flex items-center"
+    class="rounded-lg shadow-sm sticky top-0 flex items-center justify-between"
     :style="{
       background: '#fff',
-      padding: '0 0 0 10px',
       marginTop: '5px',
+      paddingLeft: '10px',
+      paddingRight: '10px',
     }"
   >
-    <div
-      v-if="collapsed"
-      class="trigger cursor-pointer hover:bg-gray-100 p-2 rounded-lg"
-      @click="$emit('toggleCollapsed')"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        viewBox="0 0 16 16"
+    <div>
+      <div
+        v-if="collapsed"
+        class="trigger cursor-pointer hover:bg-gray-100 p-2 rounded-lg"
+        @click="$emit('toggleCollapsed')"
       >
-        <path
-          fill="currentColor"
-          fill-rule="evenodd"
-          d="M11.5 2.5H13a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5h-1.5zm-1.5 0H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h7zM1 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2z"
-          clip-rule="evenodd"
-        />
-      </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill="currentColor"
+            fill-rule="evenodd"
+            d="M11.5 2.5H13a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5h-1.5zm-1.5 0H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h7zM1 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </div>
+
+      <div
+        v-else
+        class="trigger cursor-pointer hover:bg-gray-100 p-2 rounded-lg"
+        @click="$emit('toggleCollapsed')"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill="currentColor"
+            fill-rule="evenodd"
+            d="M6 2.5h7a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5H6zm-1.5 0H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h1.5zM1 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </div>
     </div>
 
-    <div
-      v-else
-      class="trigger cursor-pointer hover:bg-gray-100 p-2 rounded-lg"
-       @click="$emit('toggleCollapsed')"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        viewBox="0 0 16 16"
-      >
-        <path
-          fill="currentColor"
-          fill-rule="evenodd"
-          d="M6 2.5h7a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5H6zm-1.5 0H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h1.5zM1 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2z"
-          clip-rule="evenodd"
-        />
-      </svg>
-    </div>
+    <a-dropdown :trigger="['click']" class="cursor-pointer">
+      <a-avatar size="large" src="/avatartion.png" />
+      <template #overlay>
+        <a-menu>
+          <a-menu-item key="0">
+            <div>
+              <div class="font-bold">Antd Vue Admin</div>
+              <div class="opacity-70 text-sm">antdvueadmin@gmail.com</div>
+            </div>
+          </a-menu-item>
+          <a-menu-divider />
+          <a-menu-item key="1">
+            <div>
+              Profile
+            </div>
+          </a-menu-item>
+          <a-menu-item key="2"> Settings </a-menu-item>
+          <a-menu-divider />
+          <a-menu-item key="3">
+            <div>Logout</div>
+          </a-menu-item>
+        </a-menu>
+      </template>
+    </a-dropdown>
   </a-layout-header>
 </template>
--- src/components/common/Menus.vue
+++ src/components/common/Menus.vue
@@ -1,9 +1,16 @@
 <script lang="ts" setup>
 import {
+  SettingOutlined,
+  LockOutlined,
   UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
+  TeamOutlined,
+  LayoutOutlined,
+  BugOutlined,
 } from "@ant-design/icons-vue";
+
+import IconError404 from "@/components/icons/IconError404.vue";
+import IconServerOff from "@/components/icons/IconServerOff.vue";
+
 const selectedKeys = defineModel("selectedKeys");
 </script>
 
@@ -16,27 +23,81 @@ const selectedKeys = defineModel("selectedKeys");
       border: 'none',
     }"
   >
-    <a-menu-item key="1">
-      <user-outlined />
-      <span>nav 1</span>
-    </a-menu-item>
-    <a-menu-item key="2">
-      <video-camera-outlined />
-      <span>nav 2</span>
-    </a-menu-item>
-    <a-menu-item key="3">
-      <upload-outlined />
-      <span>nav 3</span>
-    </a-menu-item>
-    <a-sub-menu key="sub2">
+    <!-- General -->
+    <a-menu-item-group key="g1">
       <template #title>
-        <span>
-          <team-outlined />
-          <span>Team</span>
-        </span>
+        <span>General</span>
       </template>
-      <a-menu-item key="6">Team 1</a-menu-item>
-      <a-menu-item key="8">Team 2</a-menu-item>
-    </a-sub-menu>
+      <a-menu-item key="1">
+        <div class="flex items-center">
+          <layout-outlined />
+          <span>Dashboard</span>
+        </div>
+      </a-menu-item>
+      <a-menu-item key="2">
+        <div class="flex items-center">
+          <user-outlined />
+          <span>Users</span>
+        </div>
+      </a-menu-item>
+    </a-menu-item-group>
+
+    <!-- Team Management Group -->
+    <a-menu-item-group key="g2">
+      <template #title>
+        <span>Other</span>
+      </template>
+      <a-sub-menu key="sub1">
+        <template #title>
+          <div class="flex items-center">
+            <team-outlined />
+            <span>Auth</span>
+          </div>
+        </template>
+        <a-menu-item key="6">
+          <div class="flex items-center">
+            <LockOutlined />
+            <span>Sign In</span>
+          </div>
+        </a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="sub2">
+        <template #title>
+          <div class="flex items-center">
+            <BugOutlined />
+            <span>Errors</span>
+          </div>
+        </template>
+        <a-menu-item key="6">
+          <div class="flex items-center gap-2">
+            <IconError404 />
+            <span>404</span>
+          </div>
+        </a-menu-item>
+        <a-menu-item key="7">
+          <div class="flex items-center gap-2">
+            <IconServerOff />
+            <span>500</span>
+          </div>
+        </a-menu-item>
+      </a-sub-menu>
+      <a-menu-item key="sub3">
+        <div class="flex items-center">
+          <setting-outlined />
+          <span>Settings</span>
+        </div>
+      </a-menu-item>
+    </a-menu-item-group>
   </a-menu>
 </template>
+
+<style scoped>
+/* Optional: Style the group titles */
+:deep(.ant-menu-item-group-title) {
+  padding-left: 24px;
+  color: rgba(0, 0, 0, 0.45);
+  font-size: 12px;
+  font-weight: 500;
+  text-transform: uppercase;
+}
+</style>
